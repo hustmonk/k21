24a1589cc0e27afae0e1f2a1156874ba50f44f48
hustmonk <liujingminghust@163.com>
2015-06-14 08:10:34 +0800

    0.9009172908414169

--- shell/01.py
+++ shell/01.py
@@ -9,6 +9,7 @@ __revision__ = '0.1'
 
 import sys
 import math
+from common import *
 
 ps = []
 ys = []
@@ -27,15 +28,20 @@ for line in open("valid.txt.2.debug"):
 for line in open("../tovw/train2.txt.pred"):
     p, id = line.strip().split(" ")
     p = (float(p) + 1 )/2.0
-    #psd[id] = psd[id] * p
+    psd[id] = psd[id] * p
 
 ps = []
 ys = []
 for line in open("valid.txt.debug"):
     id,p,y = line.split(",")
     ys.append(int(y))
-    ps.append(float(p) * psd[id])
+    ps.append(float(p) * psd[id] * float(p))
     #ps.append(psd[id])
+    #ps.append(float(p))
 from sklearn import metrics
 roc_auc = metrics.roc_auc_score(ys, ps)
-print roc_auc
+import logging
+import logging.config
+logging.config.fileConfig("log.conf")
+logger = logging.getLogger("example")
+print logger.info(roc_auc)
